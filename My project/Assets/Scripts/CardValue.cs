53a596742d72864fb57bbfc1e90fcdf613d41a6a
Cr0usti-bat <tidianebath@gmail.com>
2024-05-25 14:20:06 +0200

    added script folder with card value

---
+++ My project/Assets/Scripts/CardValue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CardValue : MonoBehaviour
+{
+    [SerializeField] private float AttackValue =1;
+    [SerializeField] private float DefenseValue=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
