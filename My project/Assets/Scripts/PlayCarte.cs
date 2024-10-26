20a6fe297c67e63cf3d3395833093d88dd05226e
alexandre <alexandretarla@gmail.com>
2024-10-26 13:30:46 +0200

    ok

--- My project/Assets/Scripts/PlayCarte.cs
+++ My project/Assets/Scripts/PlayCarte.cs
@@ -32,7 +32,7 @@ public class PlayCarte : MonoBehaviour
 			transform.position = position;
 		}
 
-	private void OnCollisionEnter2D(Collision2D collision)
+	 void OnCollisionEnter2D(Collision2D collision)
 		{
 			//if (collision.gameObject.CompareTag("targetTag"))
 				//{
