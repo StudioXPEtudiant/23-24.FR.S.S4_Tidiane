2f8b7387c73776c986bf17612879cdb2b0899043
alexandre <alexandretarla@gmail.com>
2024-10-26 13:04:05 +0200

    ok

---
+++ My project/Assets/Scripts/PlayCarte.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCarte : MonoBehaviour
+{
+	[SerializeField] private string targetTag; 
+	[SerializeField] private GameObject CardToPlay;
+	
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+      
+    }
+
+	private void OnMouseDown()
+		{
+    		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				
+		}
+
+	private void OnMouseDrag()
+		{
+    		Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			position.z = 0;
+			transform.position = position;
+		}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+		{
+			//if (collision.gameObject.CompareTag("targetTag"))
+				//{
+					Debug.Log("w");
+				//}
+		}
+}
+//CardToPlay.
